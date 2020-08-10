7073c5fd3bc2df3a6f13fba5059ccc111e40624b
Yoshimasa Obayashi <obayashi.yoshimasa@seek-the-truth.org>
2020-08-10 11:25:38 +0900

    package/libva-v4l2-request: use autotools instead of meson

--- package/libva-v4l2-request/libva-v4l2-request.mk
+++ package/libva-v4l2-request/libva-v4l2-request.mk
@@ -5,10 +5,14 @@
 ################################################################################
 
 LIBVA_V4L2_REQUEST_VERSION = master
-LIBVA_V4L2_REQUEST_SITE = $(call github,bootlin,libva-v4l2-request,$(LIBVA_V4L2_REQUEST_VERSION))
+#LIBVA_V4L2_REQUEST_SITE = $(call github,bootlin,libva-v4l2-request,$(LIBVA_V4L2_REQUEST_VERSION))
+LIBVA_V4L2_REQUEST_SITE_METHOD = local
+LIBVA_V4L2_REQUEST_SITE = ../../V4L2/libva-v4l2-request
 LIBVA_V4L2_REQUEST_LICENSE = LGPL2.1 MIT
 LIBVA_V4L2_REQUEST_LICENSE_FILES = COPYING
 LIBVA_V4L2_REQUEST_INSTALL_STAGING = YES
+LIBVA_V4L2_REQUEST_AUTORECONF = YES
 LIBVA_V4L2_REQUEST_DEPENDENCIES = libva libdrm linux-headers
 
-$(eval $(meson-package))
+$(eval $(autotools-package))
+#$(eval $(meson-package))
